4046f39429580bd70d6cfe978fd2940e37978ca0
donRumata <rokachov@gmail.com>
2017-09-01 17:00:43 +0300

    Add double tweet test

--- src/test/java/Task01.java
+++ src/test/java/Task01.java
@@ -3,6 +3,10 @@ import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +16,8 @@ public class Task01 {
     private static final String USER_NAME = "rokachov@gmail.com";
     private static final String USER_PASS = "Rumata3330592";
     private static final String TWEET = "Hello world!";
+    private ChromeDriver driver;
+    private WebDriverWait wait;
 
     @BeforeClass
     public static void begin() {
@@ -20,17 +26,26 @@ public class Task01 {
 
     @Test
     public void chromeTest() throws InterruptedException {
-        ChromeDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(URL);
         driver.findElement(By.id("signin-email")).sendKeys(USER_NAME);
         driver.findElement(By.id("signin-password")).sendKeys(USER_PASS);
         driver.findElement(By.className("flex-table-secondary")).click();
+
+        tweet(driver);
         Thread.sleep(TIMEOUT);
-        driver.findElement(By.name("tweet")).sendKeys(TWEET);
-        driver.findElement(By.cssSelector("button.tweet-action.EdgeButton.EdgeButton--primary.js-tweet-btn")).click();
-        Thread.sleep(TIMEOUT);
+        tweet(driver);
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("message-drawer"))));
 
         driver.quit();
 
+    }
+
+    public void tweet(WebDriver driver) {
+        driver.findElement(By.name("tweet")).sendKeys(TWEET);
+        driver.findElement(By.cssSelector("button.tweet-action.EdgeButton.EdgeButton--primary.js-tweet-btn")).click();
     }
 }
