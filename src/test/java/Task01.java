b90789eddb094d217dadb712d8586eeef62d5c7a
donRumata <rokachov@gmail.com>
2017-09-04 19:28:28 +0300

    add List of tweets

--- src/test/java/Task01.java
+++ src/test/java/Task01.java
@@ -2,18 +2,22 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
 public class Task01 {
     private static final String URL = "http://www.twitter.com";
+    private static final String USER_NAME = "donrumatadon";
     private static final int TIMEOUT = 10000;
-    private static final String USER_NAME = "rokachov@gmail.com";
+    private static final String USER_LOGIN = "rokachov@gmail.com";
     private static final String USER_PASS = "";
     private static final String TWEET = "Hello world!";
     private static ChromeDriver driver;
@@ -36,12 +40,12 @@ public class Task01 {
     @Test
     public void twitterDoubleTweetCheck() throws InterruptedException {
 
-        driver.get(URL);
-        login(driver);
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("global-nav-home"))));
-        tweet(driver);
 
-        tweet(driver);
+        login(URL, USER_LOGIN, USER_PASS);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("global-nav-home"))));
+        tweet(driver, TWEET);
+        Thread.sleep(TIMEOUT);
+        tweet(driver, TWEET);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("message-drawer"))));
         String actualMessage = driver.findElement(CSS_DRAWER).getText();
 
@@ -50,19 +54,39 @@ public class Task01 {
 
     }
 
+    @Test
+    public void getAlltweetsList() {
+
+        login(URL, USER_LOGIN, USER_PASS);
+        System.out.println(getAllTweets(driver, URL, USER_NAME).size() != 0);
+    }
+
     @AfterClass
     public static void end() {
         driver.quit();
     }
 
-    private void tweet(WebDriver driver) {
-        driver.findElement(By.name("tweet")).sendKeys(TWEET);
+    private void tweet(WebDriver driver, String tweet) {
+        driver.findElement(By.name("tweet")).sendKeys(tweet);
         driver.findElement(By.cssSelector("button.tweet-action.EdgeButton.EdgeButton--primary.js-tweet-btn")).click();
     }
 
-    private void login(WebDriver driver) {
-        driver.findElement(By.id("signin-email")).sendKeys(USER_NAME);
-        driver.findElement(By.id("signin-password")).sendKeys(USER_PASS);
+    private void login(String URL, String userName, String pass) {
+        driver.get(URL);
+        driver.findElement(By.id("signin-email")).sendKeys(userName);
+        driver.findElement(By.id("signin-password")).sendKeys(pass);
         driver.findElement(By.className("flex-table-secondary")).click();
+    }
+
+    private void deleteLastTweet(WebDriver driver) {
+        driver.get(URL + "/" + USER_NAME);
+
+
+    }
+
+    private List<WebElement> getAllTweets(WebDriver driver, String URL, String USER_NAME) {
+        driver.get(URL + "/" + USER_NAME);
+        List<WebElement> result = driver.findElements(By.cssSelector("div.tweet"));
+        return result;
     }
 }
